64f3f495cac59483a7dbaab3c4ceab63493d0498
Bharat Shah <akashche72@gmail.com>
2023-06-16 16:07:54 +0545

    adding styling for the roll up

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import  "./Button.scss";
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className="btn">{props.label}</button>;
 };
 
 export default Button;
