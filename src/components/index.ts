4db111555a93f450695b9d293a220292d9523d1e
Bharat Shah <akashche72@gmail.com>
2023-06-20 17:07:54 +0545

    updating switch component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from "./Button";
 export { default as FormTemplate } from "./FromTemplate";
+export {default as BasicSwitch} from "./Switch"; 
 
 
 // Exporting types and enums which might need to use 
