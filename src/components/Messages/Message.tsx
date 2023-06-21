d7f61ea7f646413c51f93dd70152dbbc7312e304
Bharat Shah <akashche72@gmail.com>
2023-06-21 19:26:50 +0545

    redirect link is added

--- src/components/Messages/Message.tsx
+++ src/components/Messages/Message.tsx
@@ -9,17 +9,18 @@ interface SucessMessageInterface {
     buttonText: string;
     buttonVariant:variantType;
     icon:string;
+    redirectLink: string;
 }
 
 
-export default function Message({title, buttonText, buttonVariant, icon }: SucessMessageInterface) {
+export default function Message({title, buttonText, buttonVariant, icon, redirectLink }: SucessMessageInterface) {
     return (
         <div className={styles.base}>
             <div className={styles.order__completed}>
                 <img src={icon} width={82} height={82} alt={title} />
                 <div className={styles.bold__message}>{title}</div>
                 {/* <div className={styles.confirmation}> You will be receiving confirmation email</div> */}
-                <a href="/product/febric">
+                <a href={redirectLink}>
                     <Button text={buttonText} variant={buttonVariant}/>
                 </a>
             </div>
