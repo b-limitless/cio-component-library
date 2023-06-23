4f6744a2e7e98fbf83acf2a3ecc39fcdf23025b8
Bharat Shah <akashche72@gmail.com>
2023-06-23 14:49:06 +0545

    adding required component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,14 @@
 export { default as Button } from "./Button";
 export { default as FormTemplate } from "./FromTemplate";
-export {default as BasicSwitch} from "./Switch"; 
-export {default as BasicAccordion} from "./Accordion";
-export {default as Message} from "./Messages";
-export {default as Checkbox} from "./Checkbox";
-export {default as Chip} from "./Chip";
+export { default as BasicSwitch } from "./Switch";
+export { default as BasicAccordion } from "./Accordion";
+export { default as Message } from "./Messages";
+export { default as Checkbox } from "./Checkbox";
+export { default as Chip } from "./Chip";
+export { default as TextArea } from "./Textarea";
+export { default as Select } from "./Select";
+export { default as BasicTable } from "./Table";
+export { CheckboxWithLabel } from "./Checkbox/Checkbox";
 export * from "./Input";
-
-
-// Exporting types and enums which might need to use 
+// Exporting types and enums which might need to use
 export * from "./FromTemplate/form-template.types";
-
