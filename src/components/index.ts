41bbd0390d7a27f3d1a2d4570104da6841b528b0
Bharat Shah <akashche72@gmail.com>
2023-06-25 19:09:18 +0545

    exporting datatable from the central file

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,6 +9,7 @@ export { default as TextArea } from "./Textarea";
 export { default as Select } from "./Select";
 export { default as BasicTable } from "./Table";
 export { CheckboxWithLabel } from "./Checkbox/Checkbox";
+export {default as DataTable} from "./DataTable";
 export * from "./Input";
 // Exporting types and enums which might need to use
 export * from "./FromTemplate/form-template.types";
