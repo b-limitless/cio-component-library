7c538fee631489c5b1e0e2462c25bb1050979a0a
Bharat Shah <akashche72@gmail.com>
2023-06-26 16:29:24 +0545

    chip component is added

---
+++ src/components/SideModelContainer/SideModelContainer.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import styles from "./side-model-container.module.scss";
+import CloseSVG from '../../assets/svg/close.svg';
+
+
+interface SideModeInterface {
+  children: React.ReactNode;
+  showModel: boolean;
+  setShowModel: Function;
+
+}
+
+
+
+export default function SideModel({ children, showModel, setShowModel }: SideModeInterface) {
+  return (
+    <div className={`${styles.model} ${showModel ? styles.show : styles.hide}`}>
+      <div className={styles.model__side}>
+        <div className={styles.close}>
+          <CloseSVG onClick={() => setShowModel(false)}/>
+        </div>
+        {children}
+      </div>
+    </div>
+  )
+}
