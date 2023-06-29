f99da086fdf8feb18553f5eb40d0201d82e709d1
Bharat Shah <akashche72@gmail.com>
2023-06-29 17:06:37 +0545

    updating function to return first letter upppercase

--- src/functions/camelCaseToNormal.ts
+++ src/functions/camelCaseToNormal.ts
@@ -1,9 +1,16 @@
-export function camelCaseToNormal(camelCaseString: string): string {
+export function camelCaseToNormal(
+  camelCaseString: string,
+  firstLetterUpperCase?: boolean
+): string {
   // Replace capital letters with a space followed by the lowercase letter
   let normalString = camelCaseString.replace(/([A-Z])/g, " $1");
 
   // Convert the string to lowercase and trim any leading/trailing whitespace
   normalString = normalString.toLowerCase().trim();
+
+  if (firstLetterUpperCase) {
+    return `${normalString[0].toUpperCase()}${normalString.slice(1)}`;
+  }
 
   return normalString;
 }
